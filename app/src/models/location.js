1ef7e83631179227d533f2bffd26960ab4f8533f
Jason Sherman <jsherman@parcsystems.ca>
2020-05-11 14:08:07 -0700

    d'oh.  model class needs to allow null too, not just the migration script.

--- app/src/models/location.js
+++ app/src/models/location.js
@@ -120,7 +120,7 @@ module.exports = (sequelize, DataTypes) => {
       unique: false
     },
     updatedBy: {
-      allowNull: false,
+      allowNull: true,
       type: DataTypes.STRING(255),
       unique: false
     }
